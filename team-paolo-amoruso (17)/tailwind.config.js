34b8d40de9cdf169ee31c3414b4a9459acefc9c7
francescogent-beep <francesco_gent@yahoo.it>
2026-01-24 21:26:19 +0100

    Update tailwind.config.js

--- team-paolo-amoruso (17)/tailwind.config.js
+++ team-paolo-amoruso (17)/tailwind.config.js
@@ -1,3 +1,5 @@
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -20,6 +22,6 @@ export default {
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
+    typography,
   ],
 }
